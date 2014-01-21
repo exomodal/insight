9138d4d6c0dcf9af3c251e36a27df04d208093a2
sandervld <sander.veldhuis@ict.nl>
2014-01-21 13:07:07 +0100

    Only the forms for the current roles are listed in the navigation bar. Added additional authentication to routing: authenticate form.

--- client/views/includes/navigationbar.js
+++ client/views/includes/navigationbar.js
@@ -7,11 +7,30 @@
  */
 Template.navigationbar.form = function() {
 	// Get the configuration data from the collection
+	var loggedInUser = Meteor.user();
 	var config = Configuration.findOne();
+	var forms = new Array();
 
 	// Check if we got some configurations for the forms
-	if (config !== undefined && config.forms !== undefined) {
-		return config.forms;
+	if (config !== undefined && config.forms !== undefined && config.roles !== undefined &&
+			loggedInUser && loggedInUser.roles !== undefined && loggedInUser.roles[0] !== undefined) {
+
+		// Get the allowed forms
+		var allowedForms;
+		for (var i=0;i<config.roles.length;i++) {
+			if (config.roles[i].name === loggedInUser.roles[0])
+				allowedForms = config.roles[i].forms;
+		}
+
+		// Get the forms
+		for (var j=0;j<allowedForms.length;j++) {
+			for (var k=0;k<config.forms.length;k++) {
+				if (allowedForms[j] === config.forms[k].id)
+					forms.push(config.forms[k]);
+			}
+		}
+
+		return forms;
 	}
 	return undefined;
 }
