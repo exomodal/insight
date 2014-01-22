ac0d7bc6bddba87b8c11f60257d2860b5717a819
sandervld <sander.veldhuis@ict.nl>
2014-01-22 14:21:37 +0100

    Pie chart fully working. CSS added for the pie display.

--- client/views/revenues/revenues.js
+++ client/views/revenues/revenues.js
@@ -1,27 +1,12 @@
 // Variable declarations
 var MONTHS = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 var IGNORED_TAGS = ["_id","location","timestamp"];
-var DEFAULT_COLORS = ["220,0,0","0,220,0","0,0,220","220,220,0","0,220,220","220,0,220","110,0,0","0,110,0","0,0,110","110,110,0","0,110,110","110,0,110"]
+var DEFAULT_COLORS = ["220,0,0","0,220,0","0,0,220","220,220,0","0,220,220","220,0,220","110,0,0","0,110,0","0,0,110","110,110,0","0,110,110","110,0,110"];
+var DEFAULT_COLORS_HEX = ["#DC0000","#00DC00","#0000DC","#DCDC00","#00DCDC","#DC00DC","#6E0000","#006E00","#00006E","#6E6E00","#006E6E","#6E006E"];
 
 /*****************************************************************************
  * General Template function
  *****************************************************************************/
-
-Template.revenues.charts = function() {
-  var chart_type = document.getElementById('chart_type');
-  var chart_display = document.getElementById('chartDisplay');
-
-  if (chart_type && chart_type.value === "Pie") {
-    var tags = getTags();
-    var inner_html = "";
-
-    for (var i=0;i<tags;i++) {
-      inner_html += "<canvas class='chart' id='chart_" + tags[i] + "' width='200px' height='200px'></canvas>";
-    }
-  }
-  if (chart_display)
-    chart_display.innerHTML = "<canvas class='chart' id='chart' width='800px' height='200px'></canvas>"
-}
 
 Template.revenues.tags = function() {
   return getTags();
@@ -30,6 +15,26 @@ Template.revenues.tags = function() {
 /*****************************************************************************
  * General function
  *****************************************************************************/
+
+function buildCanvas() {
+  var chart_type = document.getElementById('chart_type');
+  var chart_display = document.getElementById('chartDisplay');
+
+  if (chart_type && chart_type.value === "Pie") {
+    var tags = getTags();
+    var inner_html = "";
+
+    for (var i=0;i<tags.length;i++) {
+      if ($('#'+tags[i]).attr('checked')) {
+        inner_html += "<div class='pie'><canvas class='chart' id='chart_" + tags[i] + "' width='200px' height='200px'></canvas><p>" + tags[i] + "</p></div>";
+      }
+    }
+
+    chart_display.innerHTML = inner_html;
+  } else if (chart_display) {
+    chart_display.innerHTML = "<canvas class='chart' id='chart' width='800px' height='200px'></canvas>";
+  }
+}
 
 function monthDifference(d1, d2) {
     var months;
@@ -82,6 +87,9 @@ function buildDataset(tags, data) {
 function renderGraph(start_month, start_year, end_month, end_year) {
 
   if (start_month !== '' && start_year !== '' && end_month !== '' && end_year !== '') {
+    // Set up the canvas (for line or bar) or serveral canvasses (for pie)
+    buildCanvas();
+
     // Initialize arrays
     var data = new Object();
     var labels = new Array();
@@ -127,19 +135,43 @@ function renderGraph(start_month, start_year, end_month, end_year) {
       }
     }
 
-    // Build the dataset
-    var data = {
-      labels : labels,
-      datasets : buildDataset(tags, data)
-    }
+    if (document.getElementById('chart_type').value === "Pie") {
+      for (var i=0;i<tags.length;i++) {
+        if ($('#'+tags[i]).attr('checked')) {
 
-    //Get the context of the canvas element we want to select
-    var ctx = document.getElementById("chart").getContext("2d");
+          // Build the dataset
+          var dataset = new Array();
+          var data_tag = data[tags[i]];
+          for (var j=0;j<data_tag.length;j++) {
+            dataset.push({
+              value: data_tag[j],
+              color: DEFAULT_COLORS_HEX[j]
+            });
+          }
 
-    if (document.getElementById('chart_type').value === "Bar") {
-      new Chart(ctx).Bar(data, null);
+
+          //Get the context of the canvas element we want to select
+          var ctx = document.getElementById("chart_" + tags[i]).getContext("2d");
+          // Display the pie
+          new Chart(ctx).Doughnut(dataset, null);
+        }
+      }
     } else {
-      new Chart(ctx).Line(data, null);
+      // Build the dataset
+      var data = {
+        labels : labels,
+        datasets : buildDataset(tags, data)
+      };
+
+      //Get the context of the canvas element we want to select
+      var ctx = document.getElementById("chart").getContext("2d");
+
+      // Display the bar or line
+      if (document.getElementById('chart_type').value === "Bar") {
+        new Chart(ctx).Bar(data, null);
+      } else {
+        new Chart(ctx).Line(data, null);
+      }
     }
   }
 }
