ec9c612acd4f299399039cf53118926af0dbbb6b
sandervld <sander.veldhuis@ict.nl>
2014-01-29 10:23:14 +0100

    Removed Pie charts.

--- client/components/single_graphs/single_graphs.js
+++ client/components/single_graphs/single_graphs.js
@@ -5,9 +5,6 @@ var SINGLEGRAPH_IGNORE_TAGS;
 var START_MONTH = 1;
 var START_YEAR = 2008;
 
-var START_PIE;
-var END_PIE;
-
 /*****************************************************************************
  * General Template function
  *****************************************************************************/
@@ -120,98 +117,7 @@ function buildSeries(tags, line_data) {
     }
   }
 
-  // Build the datasets for empty pie graphs.
-  // We will fill the pie data later.
-  var xAxis = 100;
-  for (var i=0;i<tags.length;i++) {
-    if (SINGLEGRAPH_IGNORE_TAGS.indexOf(tags[i]) === -1) {
-      datasets.push({
-        type:'pie',
-        name:tags[i],
-        data: [],
-        center: [xAxis, 30],
-        size:100,
-        showInLegend:false,
-        dataLabels: {
-          enabled: false
-        }
-      });
-
-      xAxis += 110;
-    }
-  }
-
   return datasets;
-}
-
-/*
- * This function updates the pie series data based
- * on the selected start and end date (navigator).
- */
-function updatePieSeries() {
-  // Initialize arrays
-  var pie_data = new Object();
-  var tags = form_tags(SINGLEGRAPH_FORM);
-  for (var i=0;i<tags.length;i++) {
-    if (SINGLEGRAPH_IGNORE_TAGS.indexOf(tags[i]) === -1) {
-      pie_data[tags[i]] = new Array();
-    }
-  }
-
-  // Calculate timestamps
-  var start_timestamp = Number(moment(START_PIE).unix() * 1000);
-  var end_timestamp = Number(moment(END_PIE).unix() * 1000);
-
-  // Parse each month
-  var month_count = monthDifference(start_timestamp, end_timestamp);
-  var month = moment.unix(start_timestamp/1000).month() + 1;
-  var year = moment.unix(start_timestamp/1000).year();
-  for (var i=0;i<month_count;i++) {
-
-    // Append data
-    var ts1 = Number(moment(month+"-"+year, "MM-YYYY").unix() * 1000);
-    var ts2 = Number(moment((month+1)+"-"+year, "MM-YYYY").unix() * 1000);
-
-    // Get the actual data
-    if (form_isLocationBound(SINGLEGRAPH_FORM)) {
-      var loc = document.getElementById('static_location').value;
-      if (loc !== "") {
-        var doc = collection_findOne(form_name(SINGLEGRAPH_FORM), {location:Number(loc), timestamp:{$gt:ts1,$lt:ts2}});
-      }
-    } else {
-      var doc = collection_findOne(form_name(SINGLEGRAPH_FORM), {timestamp:{$gt:ts1,$lt:ts2}});
-    }
-    
-    // Append data for each pie
-    for (var j=0;j<tags.length;j++) {
-      if (SINGLEGRAPH_IGNORE_TAGS.indexOf(tags[j]) === -1) {
-        if (doc && doc[tags[j]]) {
-          // If pie is in the range
-          pie_data[tags[j]].push([GLOBAL_MONTHS[month-1]+' '+year, Number(doc[tags[j]])]);
-        } else {
-          // If pie is in the range
-          pie_data[tags[j]].push([GLOBAL_MONTHS[month-1]+' '+year, 0]);
-        }
-      }
-    }
-
-    // update the counter
-    month++;
-    if (month > GLOBAL_MONTHS.length) {
-      month = 1;
-      year++;
-    }
-  }
-
-  // Update the datasets for pie graphs
-  var chart = $('#chartContainer').highcharts();
-  var j = 0;
-  for (var i=0;i<tags.length;i++) {
-    if (SINGLEGRAPH_IGNORE_TAGS.indexOf(tags[i]) === -1) {
-      chart.series[j+_.size(pie_data)].update({data:pie_data[tags[i]]});
-      j++;
-    }
-  }
 }
 
 /*
@@ -271,29 +177,10 @@ function renderGraph() {
   // Now we have all data and labels so we can display the chart
   $('#chartContainer').highcharts('StockChart', {
     rangeSelector: {selected:4,inputDateFormat:'%b %Y',inputEditDateFormat:'%b %Y'},
-    xAxis:{
-      events: {
-        setExtremes: function(e) {
-          if (e.min !== START_PIE || e.max !== END_PIE) {
-            START_PIE = e.min;
-            END_PIE = e.max;
-            Session.set("UPDATE_PIE", e.min + e.max);
-          }
-        }
-      },
-      type:'datetime',tickInterval:30*24*3600000,labels:{formatter:function() {return Highcharts.dateFormat("%b %Y", this.value);}}},
+    xAxis:{type:'datetime',tickInterval:30*24*3600000,labels:{formatter:function() {return Highcharts.dateFormat("%b %Y", this.value);}}},
     title: {text:''},
     legend: {enabled:true},
-    chart: {
-      type: 'spline',
-      events: {
-        load: function(event) {
-          START_PIE = event.currentTarget.axes[0].userMin;
-          END_PIE = event.currentTarget.axes[0].userMax;
-          Session.set("UPDATE_PIE", event.currentTarget.axes[0].userMin);
-        }
-      }
-    },
+    chart: {type: 'spline'},
     tooltip: {
       headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
       pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
@@ -304,9 +191,6 @@ function renderGraph() {
     },
     series:buildSeries(tags, line_data)
   });
-
-  // Update the pie series
-  updatePieSeries();
 }
 
 /*****************************************************************************
@@ -318,10 +202,5 @@ Template.singleGraphs.rendered = function ()  {
 
   $('#static_location').on('change', function() {
     renderGraph();
-  });
-
-  Deps.autorun(function () {
-    var update = Session.get("UPDATE_PIE");
-    updatePieSeries();
   });
 }
--- client/views/performance/performance.js
+++ client/views/performance/performance.js
@@ -7,9 +7,6 @@ var PERFORMANCE_TAGS = {"label":"Moves per Manhour",
 var PERFORMANCE_START_MONTH = 1;
 var PERFORMANCE_START_YEAR = 2008;
 
-var PERFORMANCE_START_PIE;
-var PERFORMANCE_END_PIE;
-
 /*****************************************************************************
  * General Template function
  *****************************************************************************/
@@ -83,107 +80,7 @@ function buildSeries(line_data) {
     }
   }
 
-  // Build the datasets for empty pie graphs.
-  // We will fill the pie data later.
-  var xAxis = 100;
-  if (form_isLocationBound(PERFORMANCE_TAGS.form)) {
-    var locations = config_locations();
-    for (var i=0;i<locations.length;i++) {
-      datasets.push({
-        type:'pie',
-        name:PERFORMANCE_TAGS.label + " " + locations[i].name,
-        data: [],
-        center: [xAxis, 30],
-        size:100,
-        showInLegend:false,
-        dataLabels: {
-          enabled: false
-        }
-      });
-
-      xAxis += 110;
-    }
-  }
-
   return datasets;
-}
-
-/*
- * This function updates the pie series data based
- * on the selected start and end date (navigator).
- */
-function updatePieSeries() {
-  // Initialize arrays
-  var pie_data = new Object();
-
-  if (form_isLocationBound(PERFORMANCE_TAGS.form)) {
-    var locations = config_locations();
-    for (var i=0;i<locations.length;i++) {
-      pie_data[PERFORMANCE_TAGS.label + " " + locations[i].name] = new Array();
-    }
-  } else {
-    pie_data[PERFORMANCE_TAGS.label] = new Array();
-  }
-
-  // Calculate timestamps
-  var start_timestamp = Number(moment(PERFORMANCE_START_PIE).unix() * 1000);
-  var end_timestamp = Number(moment(PERFORMANCE_END_PIE).unix() * 1000);
-
-  // Parse each month
-  var month_count = monthDifference(start_timestamp, end_timestamp);
-  var month = moment.unix(start_timestamp/1000).month() + 1;
-  var year = moment.unix(start_timestamp/1000).year();
-  for (var i=0;i<month_count;i++) {
-
-    // Append data
-    var ts1 = Number(moment(month+"-"+year, "MM-YYYY").unix() * 1000);
-    var ts2 = Number(moment((month+1)+"-"+year, "MM-YYYY").unix() * 1000);
-
-    // Get the actual data
-    if (form_isLocationBound(PERFORMANCE_TAGS.form)) {
-      var locations = config_locations();
-      for (var k=0;k<locations.length;k++) {
-
-        var doc = collection_findOne(form_name(PERFORMANCE_TAGS.form), {location:Number(locations[k].id), timestamp:{$gt:ts1,$lt:ts2}});
-        var total = 0;
-
-        for (var j=0;j<PERFORMANCE_TAGS.names.length;j++) {
-          if (doc && doc[PERFORMANCE_TAGS.names[j]]) {
-            total += Number(doc[PERFORMANCE_TAGS.names[j]]);
-          }
-        }
-
-        if (doc && doc[PERFORMANCE_TAGS.split]) {
-          total = total / Number(doc[PERFORMANCE_TAGS.split]);
-        }
-
-        pie_data[PERFORMANCE_TAGS.label + " " + locations[k].name].push([GLOBAL_MONTHS[month-1]+' '+year, total]);
-
-      }
-    } //else {
-      //var doc = collection_findOne(form_name(PERFORMANCE_TAGS.form), {timestamp:{$gt:ts1,$lt:ts2}});
-    //}
-    
-    
-
-    // update the counter
-    month++;
-    if (month > GLOBAL_MONTHS.length) {
-      month = 1;
-      year++;
-    }
-  }
-
-  // Update the datasets for pie graphs
-  var chart = $('#chartContainer').highcharts();
-  var j = 0;
-  if (form_isLocationBound(PERFORMANCE_TAGS.form)) {
-    var locations = config_locations();
-    for (var i=0;i<locations.length;i++) {
-      chart.series[j+_.size(pie_data)].update({data:pie_data[PERFORMANCE_TAGS.label + " " + locations[i].name]});
-      j++;
-    }
-  }
 }
 
 /*
@@ -251,29 +148,10 @@ function renderGraph() {
   // Now we have all data and labels so we can display the chart
   $('#chartContainer').highcharts('StockChart', {
     rangeSelector: {selected:4,inputDateFormat:'%b %Y',inputEditDateFormat:'%b %Y'},
-    xAxis:{
-      events: {
-        setExtremes: function(e) {
-          if (e.min !== PERFORMANCE_START_PIE || e.max !== PERFORMANCE_END_PIE) {
-            PERFORMANCE_START_PIE = e.min;
-            PERFORMANCE_END_PIE = e.max;
-            Session.set("UPDATE_PERFORMANCE_PIE", e.min + e.max);
-          }
-        }
-      },
-      type:'datetime',tickInterval:30*24*3600000,labels:{formatter:function() {return Highcharts.dateFormat("%b %Y", this.value);}}},
+    xAxis:{type:'datetime',tickInterval:30*24*3600000,labels:{formatter:function() {return Highcharts.dateFormat("%b %Y", this.value);}}},
     title: {text:''},
     legend: {enabled:true},
-    chart: {
-      type: 'spline',
-      events: {
-        load: function(event) {
-          PERFORMANCE_START_PIE = event.currentTarget.axes[0].userMin;
-          PERFORMANCE_END_PIE = event.currentTarget.axes[0].userMax;
-          Session.set("UPDATE_PERFORMANCE_PIE", event.currentTarget.axes[0].userMin);
-        }
-      }
-    },
+    chart: {type: 'spline'},
     tooltip: {
       headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
       pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
@@ -284,9 +162,6 @@ function renderGraph() {
     },
     series:buildSeries(line_data)
   });
-
-  // Update the pie series
-  updatePieSeries();
 }
 
 /*****************************************************************************
@@ -295,9 +170,4 @@ function renderGraph() {
 
 Template.performance.rendered = function ()  {
   renderGraph();
-
-  Deps.autorun(function () {
-    var update = Session.get("UPDATE_PERFORMANCE_PIE");
-    updatePieSeries();
-  });
 }
